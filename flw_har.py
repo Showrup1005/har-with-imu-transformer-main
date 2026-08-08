43034e347db318592e85a6c2f716073e105b820b
Showrup1005 <showrupd@gmail.com>
2026-08-08 10:36:33 +0600

    Num rounds 40 -> 60

--- flw_har.py
+++ flw_har.py
@@ -143,7 +143,7 @@ if torch.cuda.is_available():
     torch.cuda.manual_seed_all(42)
 NUM_CLIENTS = 3
 LOCAL_EPOCHS = 5
-NUM_ROUNDS = 40                   
+NUM_ROUNDS = 60                   
 
 USE_COMPRESSION = True
 COMPRESS_KEEP_RATIO_START = 0.6   # round 1
